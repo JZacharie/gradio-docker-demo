fd3a9927fe7e908f5af9f4ba94f1326e3833ff14
JZacharie <joseph@zacharie.org>
2024-01-01 10:51:45 +0100

    test blocks

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,10 @@ ARG GRADIO_SERVER_PORT=7860
 ENV GRADIO_SERVER_PORT=${GRADIO_SERVER_PORT}
 
 WORKDIR /workspace
-
-ADD requirements.txt main.py /workspace/
-
+ADD requirements.txt /workspace/
 RUN pip install -r /workspace/requirements.txt
 
 EXPOSE 7860
+ADD main.py /workspace/
 
 CMD ["python3", "/workspace/main.py"]
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!"
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-    
+with gr.Blocks() as demo:
+    gr.Audio()
+    gr.BarPlot(
+        value=simple,
+        x="item",
+        y="inventory",
+        title="Simple Bar Plot",
+        container=False,
+    )
+        
 demo.launch(server_name="0.0.0.0", server_port=7860)
