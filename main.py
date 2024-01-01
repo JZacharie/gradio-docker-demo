023fd6411b265d9bdd21bc3e90567b6e45893506
JZacharie <joseph@zacharie.org>
2024-01-01 10:58:40 +0100

    test

--- main.py
+++ main.py
@@ -2,12 +2,5 @@ import gradio as gr
 
 with gr.Blocks() as demo:
     gr.Audio()
-    gr.BarPlot(
-        value=simple,
-        x="item",
-        y="inventory",
-        title="Simple Bar Plot",
-        container=False,
-    )
-        
+
 demo.launch(server_name="0.0.0.0", server_port=7860)
